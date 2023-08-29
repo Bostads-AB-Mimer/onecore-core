c79ee16b454952c6cd3e62b8a143c51097c13948
Anders Norrback Bornholm <anders@bornholm.se>
2023-08-29 14:33:15 +0200

    Now delivers material data (step one of move to yggdrasil-property-info)

--- src/services/rental-property-service/index.ts
+++ src/services/rental-property-service/index.ts
@@ -6,7 +6,16 @@
  * course, there are always exceptions).
  */
 import KoaRouter from '@koa/router'
-import { getRentalProperty } from './adapters/rental-property-adapter'
+import {
+  getRentalProperty,
+  getRoomTypeWithMaterialOptions,
+  getSingleMaterialOption,
+  getRoomTypes,
+  getMaterialOptionGroup,
+  getMaterialOptionGroups,
+  getMaterialOption,
+  getMaterialChoices,
+} from './adapters/rental-property-adapter'
 import { getFloorPlanStream } from './adapters/document-adapter'
 
 export const routes = (router: KoaRouter) => {
@@ -14,6 +23,48 @@ export const routes = (router: KoaRouter) => {
     const response = await getFloorPlanStream(ctx.params.id)
     ctx.type = response.headers['content-type']?.toString() ?? 'image/jpeg'
     ctx.body = response.data
+  })
+
+  router.get('(.*)/rentalproperties/:id/material-options', async (ctx) => {
+    const roomTypes = await getRoomTypeWithMaterialOptions(ctx.params.id)
+
+    ctx.body = {
+      roomTypes,
+    }
+  })
+
+  router.get(
+    '(.*)/rentalproperties/:id/material-options/details',
+    async (ctx) => {
+      if (
+        ctx.request.query.roomTypeId &&
+        ctx.request.query.materialOptionGroupId &&
+        ctx.request.query.materialOptionId
+      ) {
+        const option = await getSingleMaterialOption(
+          Math.round(Math.random() * 100000).toString(),
+          ctx.request.query.roomTypeId[0],
+          ctx.request.query.materialOptionGroupId[0],
+          ctx.request.query.materialOptionId[0]
+        )
+
+        ctx.body = {
+          materialOption: option,
+        }
+      }
+    }
+  )
+
+  router.get('(.*)/rentalproperties/:id/material-choices', async (ctx) => {
+    const materialChoices = await getMaterialChoices(ctx.params.id)
+
+    ctx.body = materialChoices
+  })
+
+  router.get('(.*)/rentalproperties/:id/room-types', async (ctx) => {
+    const roomTypes = await getRoomTypes(ctx.params.id)
+
+    ctx.body = roomTypes
   })
 
   router.get('(.*)/rentalproperties/:id', async (ctx) => {
@@ -24,11 +75,37 @@ export const routes = (router: KoaRouter) => {
     }
   })
 
-  /*router.get('(.*)/rentalproperties/:id', async (ctx) => {
-    const responseData = await getRentalProperties()
+  router.get(
+    '(.*)/room-types/:roomTypeId/material-option-groups/:optionGroupId',
+    async (ctx) => {
+      const group = await getMaterialOptionGroup(
+        ctx.params.roomTypeId,
+        ctx.params.optionGroupId
+      )
 
-    ctx.body = {
-      data: responseData,
+      ctx.body = group
     }
-  })*/
+  )
+
+  router.get(
+    '(.*)/room-types/:roomTypeId/material-option-groups',
+    async (ctx) => {
+      const groups = await getMaterialOptionGroups(ctx.params.roomTypeId)
+
+      ctx.body = groups
+    }
+  )
+
+  router.get(
+    '(.*)/room-types/:roomTypeId/material-option-groups/:optionGroupId/options/:optionId',
+    async (ctx) => {
+      const option = await getMaterialOption(
+        ctx.params.roomTypeId,
+        ctx.params.optionGroupId,
+        ctx.params.optionId
+      )
+
+      ctx.body = option
+    }
+  )
 }
