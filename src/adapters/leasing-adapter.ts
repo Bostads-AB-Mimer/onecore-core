2bce42e00e4f2955979d1cf832c39206f2f1f53a
Anders Norrback Bornholm <anders@bornholm.se>
2024-02-27 14:27:27 +0200

    Started restructuring project. Added tests for parking space processes

---
+++ src/adapters/leasing-adapter.ts
@@ -0,0 +1,86 @@
+import axios from 'axios'
+import { ConsumerReport, Contact, Lease } from 'onecore-types'
+import config from '../common/config'
+
+const tenantsLeasesServiceUrl = config.tenantsLeasesService.url
+
+const getLease = async (leaseId: string): Promise<Lease> => {
+  const leaseResponse = await axios(
+    tenantsLeasesServiceUrl + '/leases/' + leaseId
+  )
+
+  return leaseResponse.data.data
+}
+
+const getLeasesForPnr = async (
+  nationalRegistrationNumber: string
+): Promise<Lease[]> => {
+  const leasesResponse = await axios(
+    tenantsLeasesServiceUrl + '/leases/for/' + nationalRegistrationNumber
+  )
+
+  return leasesResponse.data.data
+}
+
+const getContactForPnr = async (
+  nationalRegistrationNumber: string
+): Promise<Contact> => {
+  const contactResponse = await axios(
+    tenantsLeasesServiceUrl + '/contact/' + nationalRegistrationNumber
+  )
+
+  return contactResponse.data.data
+}
+
+const getContact = async (contactId: string): Promise<Contact | undefined> => {
+  try {
+    const contactResponse = await axios(
+      tenantsLeasesServiceUrl + '/contact/contactCode/' + contactId
+    )
+
+    return contactResponse.data.data
+  } catch (error) {
+    return undefined
+  }
+}
+
+const createLease = async (
+  objectId: string,
+  contactId: string,
+  fromDate: string,
+  companyCode: string
+) => {
+  const axiosOptions = {
+    method: 'POST',
+    data: {
+      parkingSpaceId: objectId,
+      contactCode: contactId,
+      fromDate,
+      companyCode,
+    },
+  }
+
+  const result = await axios(tenantsLeasesServiceUrl + '/leases', axiosOptions)
+
+  return result.data
+}
+
+const getCreditInformation = async (
+  nationalRegistrationNumber: string
+): Promise<ConsumerReport> => {
+  const informationResponse = await axios(
+    tenantsLeasesServiceUrl +
+      '/cas/getConsumerReport/' +
+      nationalRegistrationNumber
+  )
+  return informationResponse.data.data
+}
+
+export {
+  getLease,
+  getLeasesForPnr,
+  getContactForPnr,
+  getContact,
+  createLease,
+  getCreditInformation,
+}
--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,6 @@
 import KoaRouter from '@koa/router'
 import { routes as leaseRoutes } from './services/lease-service'
-import { routes as rentalPropertyRoutes } from './services/rental-property-service'
+import { routes as rentalPropertyRoutes } from './services/property-management-service'
 
 const router = new KoaRouter()
 
