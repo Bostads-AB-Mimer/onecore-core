859d2d2af06b6c3167e408a74ba89570d9efca86
momentiris <momentiris@gmail.com>
2024-05-28 12:23:46 +0200

    scaffold process

--- src/processes/parkingspaces/internal/create-offer.ts
+++ src/processes/parkingspaces/internal/create-offer.ts
@@ -1,4 +1,7 @@
+import { ApplicantStatus, OfferStatus } from 'onecore-types'
+
 import { ProcessResult, ProcessStatus } from '../../../common/types'
+import * as leasingAdapter from '../../../adapters/leasing-adapter'
 
 export const createOfferForInternalParkingSpace = async (
   listingId: string
@@ -11,11 +14,40 @@ export const createOfferForInternalParkingSpace = async (
 
   try {
     // step 0 - validation: listing is no longer published and status is AdvertisementEnded
+    const listing = await leasingAdapter.getListingByListingId(listingId)
+    if (!listing) throw new Error('no listing')
+    // What is ListingStatus?
+    // What is AdvertisementEnded?
+    if (new Date(listing.publishedTo) > new Date())
+      throw new Error('listing still published')
+
     // step 1 - get list of applicants
     // step 2 - sort applicants by rental criteria
     // step 3 - check for valid applicants
+    const applicants =
+      (await leasingAdapter.getListingByIdWithDetailedApplicants(
+        String(listing.id)
+      )) ?? []
+
+    const [applicant] = applicants
+    if (!applicant) throw new Error('no applicant')
+
     // step 3a - create offer
+    await leasingAdapter.createOffer({
+      applicantId: applicant.id,
+      expiresAt: new Date(),
+      listingId: listing.id,
+      selectedApplicants: applicants,
+      status: OfferStatus.Active,
+    })
+
     // step 4 - update status of winning applicant
+    await leasingAdapter.updateApplicantStatus({
+      applicantId: applicant.id,
+      contactCode: applicant.contactCode,
+      status: ApplicantStatus.Active,
+    })
+
     // step 5 - notify winning applicant
 
     return {
