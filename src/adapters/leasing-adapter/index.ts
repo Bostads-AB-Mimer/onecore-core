fc92669e674fe8864134a70ae2c2e11940bb91f5
momentiris <momentiris@gmail.com>
2024-10-23 20:17:24 +0200

    extract offer fns into file

--- src/adapters/leasing-adapter/index.ts
+++ src/adapters/leasing-adapter/index.ts
@@ -1,5 +1,5 @@
 import { loggedAxios as axios, logger } from 'onecore-utilities'
-import { AxiosError, HttpStatusCode } from 'axios'
+import { AxiosError } from 'axios'
 import dayjs from 'dayjs'
 import {
   ConsumerReport,
@@ -11,14 +11,8 @@ import {
   Applicant,
   ApplicantStatus,
   ApplicantWithListing,
-  Offer,
   DetailedApplicant,
-  OfferWithRentalObjectCode,
-  DetailedOffer,
   Tenant,
-  CreateOfferParams,
-  OfferWithOfferApplicants,
-  GetActiveOfferByListingIdErrorCodes,
 } from 'onecore-types'
 
 import { AdapterResult } from './../types'
@@ -429,88 +423,6 @@ const withdrawApplicantByUser = async (
   }
 }
 
-const createOffer = async (
-  params: CreateOfferParams
-): Promise<AdapterResult<Omit<Offer, 'selectedApplicants'>, 'unknown'>> => {
-  try {
-    const response = await axios.post<{ content: Offer }>(
-      `${tenantsLeasesServiceUrl}/offer`,
-      params
-    )
-
-    return { ok: true, data: response.data.content }
-  } catch (err) {
-    logger.error(err, 'Error creating offer:')
-    return { ok: false, err: 'unknown' }
-  }
-}
-
-const getOfferByOfferId = async (
-  offerId: number
-): Promise<AdapterResult<DetailedOffer, 'not-found' | 'unknown'>> => {
-  try {
-    const res = await axios(`${tenantsLeasesServiceUrl}/offers/${offerId}`)
-
-    if (res.status == HttpStatusCode.NotFound) {
-      return { ok: false, err: 'not-found' }
-    }
-    return { ok: true, data: res.data.content }
-  } catch (err) {
-    logger.error({ err }, 'leasing-adapter.getOfferByOfferId')
-    return { ok: false, err: 'unknown' }
-  }
-}
-
-const getOffersByListingId = async (
-  listingId: number
-): Promise<AdapterResult<Array<OfferWithOfferApplicants>, 'unknown'>> => {
-  try {
-    const res = await axios(
-      `${tenantsLeasesServiceUrl}/offers/listing-id/${listingId}`
-    )
-
-    return { ok: true, data: res.data.content }
-  } catch (err) {
-    logger.error({ err }, 'leasing-adapter.getOffersByListingId')
-    return { ok: false, err: 'unknown' }
-  }
-}
-
-const getActiveOfferByListingId = async (
-  listingId: number
-): Promise<AdapterResult<Offer, GetActiveOfferByListingIdErrorCodes>> => {
-  try {
-    const res = await axios(
-      `${tenantsLeasesServiceUrl}/offers/listing-id/${listingId}/active`
-    )
-
-    if (!res.data.content) {
-      return {
-        ok: false,
-        err: GetActiveOfferByListingIdErrorCodes.NotFound,
-        statusCode: res.status,
-      }
-    }
-
-    return { ok: true, data: res.data.content, statusCode: res.status }
-  } catch (err) {
-    logger.error({ err }, 'leasing-adapter.getActiveOfferByListingId')
-    if (err instanceof AxiosError) {
-      return {
-        ok: false,
-        err: GetActiveOfferByListingIdErrorCodes.Unknown,
-        statusCode: err.response?.status ?? 500,
-      }
-    } else {
-      return {
-        ok: false,
-        err: GetActiveOfferByListingIdErrorCodes.Unknown,
-        statusCode: 500,
-      }
-    }
-  }
-}
-
 const updateApplicantStatus = async (params: {
   contactCode: string
   applicantId: number
@@ -525,46 +437,6 @@ const updateApplicantStatus = async (params: {
   } catch (err) {
     logger.error(err, 'Error updating applicant status')
     throw err
-  }
-}
-
-const getOffersForContact = async (
-  contactCode: string
-): Promise<
-  AdapterResult<OfferWithRentalObjectCode[], 'not-found' | 'unknown'>
-> => {
-  try {
-    const res = await axios(
-      `${tenantsLeasesServiceUrl}/contacts/${contactCode}/offers`
-    )
-
-    if (res.status == HttpStatusCode.NotFound) {
-      return { ok: false, err: 'not-found' }
-    }
-
-    return { ok: true, data: res.data.content }
-  } catch (err) {
-    logger.error({ err }, 'leasing-adapter.getOffersForContact')
-    return { ok: false, err: 'unknown' }
-  }
-}
-
-const getOfferByContactCodeAndOfferId = async (
-  contactCode: string,
-  offerId: string
-): Promise<AdapterResult<DetailedOffer, 'not-found' | 'unknown'>> => {
-  try {
-    const res = await axios(
-      `${tenantsLeasesServiceUrl}/offers/${offerId}/applicants/${contactCode}`
-    )
-
-    if (res.status == HttpStatusCode.NotFound) {
-      return { ok: false, err: 'not-found' }
-    }
-    return { ok: true, data: res.data.content }
-  } catch (err) {
-    logger.error({ err }, 'leasing-adapter.getOffersForContact')
-    return { ok: false, err: 'unknown' }
   }
 }
 
@@ -652,54 +524,6 @@ const validatePropertyRentalRules = async (
   }
 }
 
-async function closeOfferByAccept(
-  offerId: number
-): Promise<AdapterResult<null, 'offer-not-found' | 'unknown'>> {
-  const res = await axios.put(
-    `${tenantsLeasesServiceUrl}/offers/${offerId}/close-by-accept`
-  )
-
-  if (res.status === 200) {
-    return { ok: true, data: null }
-  }
-
-  if (res.status === 404) {
-    return { ok: false, err: 'offer-not-found' }
-  }
-
-  return { ok: false, err: 'unknown' }
-}
-
-async function closeOfferByDeny(
-  offerId: number
-): Promise<AdapterResult<null, 'offer-not-found' | 'unknown'>> {
-  const res = await axios.put(
-    `${tenantsLeasesServiceUrl}/offers/${offerId}/deny`
-  )
-
-  if (res.status === 200) {
-    return { ok: true, data: null }
-  }
-
-  if (res.status === 404) {
-    return { ok: false, err: 'offer-not-found' }
-  }
-
-  return { ok: false, err: 'unknown' }
-}
-
-const handleExpiredOffers = async (): Promise<
-  AdapterResult<null | number[], 'unknown'>
-> => {
-  const res = await axios.put(`${tenantsLeasesServiceUrl}/offers/handleexpired`)
-
-  if (res.status === 200) {
-    return { ok: true, data: res.data.content }
-  } else {
-    return { ok: false, err: 'unknown' }
-  }
-}
-
 export {
   getLease,
   getLeasesForPnr,
@@ -720,21 +544,33 @@ export {
   withdrawApplicantByManager,
   withdrawApplicantByUser,
   setApplicantStatusActive,
-  createOffer,
   updateApplicantStatus,
-  getOffersForContact,
-  getOfferByOfferId,
   getContactsDataBySearchQuery,
   getContactByContactCode,
-  getOfferByContactCodeAndOfferId,
   validateResidentialAreaRentalRules,
   validatePropertyRentalRules,
   getTenantByContactCode,
-  closeOfferByAccept,
-  getOffersByListingId,
-  closeOfferByDeny,
-  handleExpiredOffers,
-  getActiveOfferByListingId,
 }
 
-export * from './listings'
+export {
+  applyForListing,
+  createNewListing,
+  deleteListing,
+  getListingByListingId,
+  getListingByRentalObjectCode,
+  getListingsWithApplicants,
+  syncInternalParkingSpacesFromXpand,
+  updateListingStatus,
+} from './listings'
+
+export {
+  closeOfferByAccept,
+  closeOfferByDeny,
+  createOffer,
+  getActiveOfferByListingId,
+  getOfferByContactCodeAndOfferId,
+  getOfferByOfferId,
+  getOffersByListingId,
+  getOffersForContact,
+  handleExpiredOffers,
+} from './offers'
--- src/adapters/tests/leasing-adapter/index.test.ts
+++ src/adapters/tests/leasing-adapter/index.test.ts
@@ -1,7 +1,6 @@
 import { HttpStatusCode } from 'axios'
 import assert from 'node:assert'
 import nock from 'nock'
-import { OfferStatus } from 'onecore-types'
 
 import config from '../../../common/config'
 import * as leasingAdapter from '../../leasing-adapter'
@@ -143,79 +142,6 @@ describe('leasing-adapter', () => {
 
       expect(result.ok).toEqual(false)
       if (!result.ok) expect(result.err).toBe('unknown')
-    })
-  })
-
-  describe(leasingAdapter.createOffer, () => {
-    it('should try to create an offer', async () => {
-      nock(config.tenantsLeasesService.url)
-        .post('/offer')
-        .reply(201, { content: { id: 1 } })
-
-      const result = await leasingAdapter.createOffer({
-        expiresAt: new Date(),
-        listingId: 1,
-        applicantId: 1,
-        selectedApplicants: [],
-        status: 1,
-      })
-      assert(result.ok)
-
-      expect(result.data).toEqual({ id: 1 })
-    })
-  })
-
-  describe(leasingAdapter.getOfferByOfferId, () => {
-    it('should try to get offer', async () => {
-      nock(config.tenantsLeasesService.url)
-        .get('/offers/123')
-        .reply(200, {
-          content: factory.detailedOffer.build({
-            id: 123,
-            status: OfferStatus.Active,
-            listingId: 456,
-          }),
-        })
-
-      const res = await leasingAdapter.getOfferByOfferId(123)
-
-      expect(res.ok).toBe(true)
-      if (res.ok) {
-        expect(res.data.listingId).toBe(456)
-        expect(res.data.status).toBe(OfferStatus.Active)
-        expect(res.data.id).toBe(123)
-      }
-    })
-
-    it('should return 404 on not found', async () => {
-      nock(config.tenantsLeasesService.url)
-        .get('/offers/123')
-        .reply(404, {
-          content: {
-            ok: false,
-            err: 'not-found',
-          },
-        })
-
-      const res = await leasingAdapter.getOfferByOfferId(123)
-
-      expect(res.ok).toBe(false)
-      if (!res.ok) expect(res.err).toBe('not-found')
-    })
-  })
-
-  describe(leasingAdapter.getOffersForContact, () => {
-    it('should try to get offers', async () => {
-      const offer = factory.offerWithRentalObjectCode.build()
-      nock(config.tenantsLeasesService.url)
-        .get('/contacts/P174965/offers')
-        .reply(200, {
-          content: [offer],
-        })
-
-      const result = await leasingAdapter.getOffersForContact('P174965')
-      assert(result.ok)
-      expect(result.data).toEqual([expect.objectContaining({ id: offer.id })])
     })
   })
 
