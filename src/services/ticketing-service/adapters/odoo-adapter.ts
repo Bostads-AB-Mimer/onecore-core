a48c5470f6568240af06a79226921d1ba6365ea7
Johannes <erikjohanneskarlsson@gmail.com>
2024-10-28 14:47:53 +0100

    fix: changes message type of mimer messages in ticketing service

--- src/services/ticketing-service/adapters/odoo-adapter.ts
+++ src/services/ticketing-service/adapters/odoo-adapter.ts
@@ -277,7 +277,7 @@ const addMessageToTicket = async (
     res_id: ticketId,
     model: 'maintenance.request',
     body: striptags(message.body).replaceAll('\n', '<br>'),
-    message_type: 'notification',
+    message_type: 'inbox',
   })
 }
 
