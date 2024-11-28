990a2bf4264e83cc190a100e4a87dd21109cea6d
driatic <fredrik.hasselgren@prototyp.se>
2024-11-28 12:50:36 +0100

    UTH-137: update mappings for max number of residents in application

--- src/services/lease-service/services/is-allowed-num-residents.ts
+++ src/services/lease-service/services/is-allowed-num-residents.ts
@@ -1,6 +1,6 @@
 const maxResidents: Record<string, number> = {
   '1R': 3,
-  '1RKV': 3,
+  '1RKV': 2,
   '1RK': 3,
   '1RKSO': 3,
   '1RKM': 3,
@@ -19,14 +19,14 @@ const maxResidents: Record<string, number> = {
   '4RKAL': 7,
   '5RK': 8,
   '5RKM': 8,
-  '6RK': 9,
-  ATELJE: 8,
-  '8RK': 8,
-  '6RKM': 9,
-  '1RKOK': 1,
-  SAMLGH: 1,
-  STUD: 1,
-  '6RKE': 9,
+  '6RK': 10,
+  ATELJE: 8, //do these exists? check in db - no cap in excel file
+  '8RK': 8, // does not exist according to excel file
+  '6RKM': 10,
+  '1RKOK': 2,
+  SAMLGH: 1, //not rented by Mimer but by the municipality
+  STUD: 1, //rented by Bostad Vås
+  '6RKE': 10,
   '5RKE': 8,
   '4RKE': 7,
   '3RKE': 5,
@@ -36,12 +36,12 @@ const maxResidents: Record<string, number> = {
   '2RKS': 4,
   '2RKVS': 4,
   '2RKVMS': 4,
-  '1RKDEL': 2,
+  '1RKDEL': 2, //does not exist within Mimer, rented by Bostad Vås
   '3RKV': 5,
   '3RKS': 5,
-  '1,5 R': 2,
-  '2,5 ROK': 4,
-  '3,5ROK': 5,
+  '1,5 R': 2, //does not exist within Mimer
+  '2,5 ROK': 4, //does not exist within Mimer
+  '3,5ROK': 5, //does not exist within Mimer
 }
 
 export function isAllowedNumResidents(
