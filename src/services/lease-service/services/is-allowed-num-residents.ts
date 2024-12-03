73298377b1e5a2c1b26f4cae3bb34778ec9d799c
Fredrik Hasselgren <fredrik.hasselgren@prototyp.se>
2024-12-03 10:17:04 +0100

    UTH-137: update mappings for max number of residents in application (#226)
    * UTH-137: update mappings for max number of residents in application
    
    * UTH-137: clarify comments
    
    * UTH-137: separate types that Mimer does not rent out

--- src/services/lease-service/services/is-allowed-num-residents.ts
+++ src/services/lease-service/services/is-allowed-num-residents.ts
@@ -1,6 +1,6 @@
 const maxResidents: Record<string, number> = {
   '1R': 3,
-  '1RKV': 3,
+  '1RKV': 2,
   '1RK': 3,
   '1RKSO': 3,
   '1RKM': 3,
@@ -19,14 +19,10 @@ const maxResidents: Record<string, number> = {
   '4RKAL': 7,
   '5RK': 8,
   '5RKM': 8,
-  '6RK': 9,
-  ATELJE: 8,
-  '8RK': 8,
-  '6RKM': 9,
-  '1RKOK': 1,
-  SAMLGH: 1,
-  STUD: 1,
-  '6RKE': 9,
+  '6RK': 10,
+  '6RKM': 10,
+  '1RKOK': 2,
+  '6RKE': 10,
   '5RKE': 8,
   '4RKE': 7,
   '3RKE': 5,
@@ -36,12 +32,18 @@ const maxResidents: Record<string, number> = {
   '2RKS': 4,
   '2RKVS': 4,
   '2RKVMS': 4,
-  '1RKDEL': 2,
   '3RKV': 5,
   '3RKS': 5,
+
+  //property types that exists in Xpand but not in Mimer
+  ATELJE: 8,
+  SAMLGH: 1,
+  STUD: 1,
   '1,5 R': 2,
+  '1RKDEL': 2,
   '2,5 ROK': 4,
   '3,5ROK': 5,
+  '8RK': 8,
 }
 
 export function isAllowedNumResidents(
