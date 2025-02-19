51ca17b06bd7febabc95a10aa4b036807c181420
Andreas Lundqvist <momentiris@gmail.com>
2025-02-19 15:16:28 +0100

    put route helpers in file

--- src/services/lease-service/index.ts
+++ src/services/lease-service/index.ts
@@ -7,7 +7,7 @@
  */
 import KoaRouter from '@koa/router'
 import dayjs from 'dayjs'
-import { GetActiveOfferByListingIdErrorCodes, leasing } from 'onecore-types'
+import { GetActiveOfferByListingIdErrorCodes } from 'onecore-types'
 import { logger, generateRouteMetadata } from 'onecore-utilities'
 import { z } from 'zod'
 
@@ -16,6 +16,7 @@ import * as propertyManagementAdapter from '../../adapters/property-management-a
 import { ProcessStatus } from '../../common/types'
 import { parseRequestBody } from '../../middlewares/parse-request-body'
 import * as internalParkingSpaceProcesses from '../../processes/parkingspaces/internal'
+import { makeAdminApplicationProfileRequestParams } from './helpers/application-profile'
 import { schemas } from './schemas'
 import { isAllowedNumResidents } from './services/is-allowed-num-residents'
 
@@ -1563,104 +1564,6 @@ export const routes = (router: KoaRouter) => {
     typeof schemas.admin.applicationProfile.UpdateApplicationProfileRequestParams
   >
 
-  function getDiffType(
-    params: UpdateAdminApplicationProfileRequestParams,
-    existing: leasingAdapter.GetApplicationProfileResponseData
-  ): 'profile' | 'review' | 'both' | 'neither' {
-    const { housingReference: incomingHousingReference, ...incomingProfile } =
-      params
-    const { housingReference: existingHousingReference, ...existingProfile } =
-      schemas.admin.applicationProfile.UpdateApplicationProfileRequestParams.parse(
-        existing
-      )
-
-    const reviewed =
-      incomingHousingReference.reviewStatus !==
-      existingHousingReference.reviewStatus
-
-    const profileUpdate =
-      JSON.stringify(incomingProfile) !== JSON.stringify(existingProfile)
-
-    if (reviewed && profileUpdate) {
-      return 'both'
-    } else if (reviewed) {
-      return 'review'
-    } else if (profileUpdate) {
-      return 'profile'
-    } else {
-      return 'neither'
-    }
-  }
-
-  function makeAdminApplicationProfileRequestParams(
-    body: UpdateAdminApplicationProfileRequestParams,
-    existingProfile?: leasingAdapter.GetApplicationProfileResponseData
-  ): leasingAdapter.CreateOrUpdateApplicationProfileRequestParams {
-    const now = new Date()
-    const expiresAt = dayjs(now).add(6, 'months').toDate()
-
-    if (!existingProfile) {
-      return {
-        ...body,
-        lastUpdatedAt: null,
-        expiresAt: null,
-        housingReference: {
-          ...body.housingReference,
-          expiresAt: null,
-          reviewedAt:
-            body.housingReference.reviewStatus === 'PENDING' ? null : now,
-        },
-      }
-    }
-
-    const diffType = getDiffType(body, existingProfile)
-    if (diffType === 'neither') {
-      return {
-        ...body,
-        lastUpdatedAt: existingProfile.lastUpdatedAt,
-        expiresAt: existingProfile.expiresAt,
-        housingReference: {
-          ...body.housingReference,
-          reviewedAt: existingProfile.housingReference.reviewedAt,
-          expiresAt: existingProfile.housingReference.expiresAt,
-        },
-      }
-    } else if (diffType === 'profile') {
-      return {
-        ...body,
-        expiresAt,
-        lastUpdatedAt: now,
-        housingReference: {
-          ...body.housingReference,
-          reviewedAt: existingProfile.housingReference.reviewedAt,
-          expiresAt: existingProfile.housingReference.expiresAt,
-        },
-      }
-    } else if (diffType === 'review') {
-      return {
-        ...body,
-        lastUpdatedAt: existingProfile.lastUpdatedAt,
-        expiresAt: existingProfile.expiresAt,
-        housingReference: {
-          ...body.housingReference,
-          expiresAt,
-          reviewedAt: now,
-        },
-      }
-    } else {
-      return {
-        ...body,
-        expiresAt,
-        lastUpdatedAt: now,
-        housingReference: {
-          ...body.housingReference,
-          expiresAt,
-          reviewedAt: now,
-        },
-      }
-    }
-  }
-
   router.post(
     '(.*)/contacts/:contactCode/application-profile/admin',
     parseRequestBody(
@@ -1686,6 +1589,7 @@ export const routes = (router: KoaRouter) => {
         ctx.body = { error: 'unknown', ...metadata }
         return
       }
+
       const createOrUpdate =
         await leasingAdapter.createOrUpdateApplicationProfileByContactCode(
           ctx.params.contactCode,
