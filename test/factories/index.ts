837b168e8d6488c8ef0798d29e4ee850e0722374
Andreas Lundqvist <31646645+momentiris@users.noreply.github.com>
2025-05-28 08:59:32 +0200

    MIM-506 (#299)
    * use correct schema types for work order adapter and endpoints
    
    * adapt routes, adapter and tests for previous changes
    
    * wip add duedate
    
    * add duedate to xpand work order schema
    
    * add back url

--- test/factories/index.ts
+++ test/factories/index.ts
@@ -13,13 +13,16 @@ export { RentalPropertyInfoFactory as rentalPropertyInfo } from './rental-proper
 export { TenantFactory as tenant } from './tenant'
 export {
   WorkOrderFactory as workOrder,
+  XpandWorkOrderFactory as xpandWorkOrder,
+  XpandWorkOrderDetailsFactory as xpandWorkOrderDetails,
   CreateWorkOrderFactory as createWorkOrder,
   CreateWorkOrderDetailsFactory as createWorkOrderDetails,
 } from './work-order'
 export {
-  XpandWorkOrderFactory as xpandWorkOrder,
-  XpandWorkOrderDetailsFactory as xpandWorkOrderDetails,
-} from './xpand-work-order'
+  XpandWorkOrderFactory as externalXpandWorkOrder,
+  XpandWorkOrderDetailsFactory as externalXpandWorkOrderDetails,
+  OdooWorkOrderFactory as externalOdooWorkOrder,
+} from './work-order-external'
 export { MaintenanceUnitInfoFactory as maintenanceUnitInfo } from './maintenance-unit-info'
 export {
   ApplicationProfileFactory as applicationProfile,
